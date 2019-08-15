c487527e662a83cda3b5fb25e0877d23d20d7e07
kartikay-bagla <kartikaybagla@gmail.com>
2019-08-15 22:40:21 +0530

    Got the scroll button working

--- home_page_js.js
+++ home_page_js.js
@@ -1,5 +1,5 @@
-let arrow = document.querySelector('.arrow_img');
-
-arrow.addEventListener('click',e=>{
-    document.querySelector('#dummy_link').click();
+$("#arrow").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#seg_two").offset().top - 60
+    }, 500);
 });
